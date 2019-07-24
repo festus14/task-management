fd1c6affda540c02fd2e9049208cb037196e8e73
Ayonitemi Sosanya <ayonitemi.sosanya@intranet.com>
2019-07-24 10:56:23 -0700

    comment added

--- public/metro/comment.js
+++ public/metro/comment.js
@@ -15,58 +15,86 @@ function getFormattedDate(date) {
 var data = [{
         "id": 2,
         name: "Yeha",
-        "comment": "laudantium enim"
+        date: "12/2/2000",
+        "comment": "laudantium enim ladugbo mi ncicna jnsjkd cfjaka"
     },
-    // {
-    //     "id": 1,
-    //     name: "Ya",
-    //     "comment": "est natus enim nihil "
-    // },
+    {
+        "id": 1,
+        name: "Ya",
+        date: "2/3/2019",
+        "comment": "est natus enim nihil"
+    },
 ]
 mapComment();
-var comArray = [];
 
 
 function mapComment() {
     data.map((elem, i) => {
         console.log(elem.comment)
-        var html = elem.id === 1 ? `<div class="m-messenger__wrapper commguy" style="padding-right: 10px; padding-left: 10px;">
-        <div class="m-messenger__message m-messenger__message--out">
-        <div class="m-messenger__message-pic">
-        <img src="assets/app/media/img//users/user3.jpg" alt="" class="mCS_img_loaded">
-    </div>
-            <div class="m-messenger__message-body">
-                <div class="m-messenger__message-arrow"></div>
-                <div class="m-messenger__message-content">
-                <div class="m-messenger__message-username">
-                    <div class="m-messenger__message-text" style="  max-width: 440px; max-height: 4000px;>
-                        <span id="datee" style="float: right;"></span>
-                         ${elem.comment}
-                         </br>
-                         <i class="fa fa-reply" aria-hidden="true" style="float:right; margin-top:1px; margin-bottom: 2px; height:1px;"></i>
-                    </div>
-                    <div class="m-messenger__message-text">
-                    <span id="datee" style="float: right;"></span> ${elem.comment}
-                </div>
-                </div>
-            </div>
-        </div>
-    </div>` : `<div class="m-messenger__wrapper commguy" style="padding-right: 10px; padding-left: 10px;">
+        var html = elem.id === 1 ?
+            //     `<div class="m-messenger__wrapper commguy" style="padding-right: 10px; padding-left: 10px;">
+            //     <div class="m-messenger__message m-messenger__message--out">
+            //     <div class="m-messenger__message-pic">
+            //     <img src="assets/app/media/img//users/user3.jpg" alt="" class="mCS_img_loaded">
+            // </div>
+            //         <div class="m-messenger__message-body">
+            //             <div class="m-messenger__message-arrow"></div>
+            //             <div class="m-messenger__message-content">
+            //             <div class="m-messenger__message-username">
+            //                 <div class="m-messenger__message-text" style="  max-width: 440px; max-height: 4000px;>
+            //                     <span id="datee" style="float: right;"></span>
+            //                      ${elem.comment}
+            //                      </br>
+            //                      <i class="fa fa-reply" aria-hidden="true" style="float:right; margin-top:1px; margin-bottom: 2px; height:1px;"></i>
+            //                 </div>
+            //                 <div class="m-messenger__message-text">
+            //                 <span id="datee" style="float: right;"></span> ${elem.comment}
+            //             </div>
+            //             </div>
+            //         </div>
+            //     </div>
+            // </div>` 
+            `<div class="m-messenger__wrapper commguy" style="padding-right: 10px; padding-left: 10px;">
     <div class="m-messenger__message m-messenger__message--out">
     
         <div class="m-messenger__message-body">
             <div class="m-messenger__message-arrow"></div>
             <div class="m-messenger__message-content">
             <div class="m-messenger__message-username">
-            <span id="datee" style="float: right;"></span>
-                ${elem.name}
+            <span class="secondary"><strong>${elem.name}</strong></span>
+            <span id="datee" style="float: right;">${elem.date}</span>
+               
                 </div>
-                <div class="m-messenger__message-text" style="  max-width: 440px; max-height: 4000px;">
-                    <span id="datee" style="float: right;"></span> ${elem.comment}
+                <div class="m-messenger__message-text" style="  min-width: 250px; max-width: 440px; max-height: 4000px;">
+                    ${elem.comment}
                 </div>
                 </br>
                 <i class="fa fa-reply" aria-hidden="true" style="float:right; margin-top:1px; margin-bottom: 2px; height:1px;"></i>
-           
+           </br>
+            </div>
+        </div>
+        <div class="m-messenger__message-pic">
+        <img src="assets/app/media/img//users/user3.jpg" alt="" class="mCS_img_loaded">
+    </div>
+    </div>
+</div>` :
+            `<div class="m-messenger__wrapper commguy" style="padding-right: 10px; padding-left: 10px;">
+    <div class="m-messenger__message m-messenger__message--out">
+    
+        <div class="m-messenger__message-body">
+            <div class="m-messenger__message-arrow"></div>
+            <div class="m-messenger__message-content">
+            <div class="m-messenger__message-username">
+            <span class="secondary"><strong>${elem.name}</strong></span>
+            <span id="datee" style="float: right;">${elem.date}</span>
+               
+                </div>
+                <div class="m-messenger__message-text" style=" min-width: 250px; max-width: 440px; max-height: 4000px;">
+                    ${elem.comment}
+                </div>
+                </br>
+                <i class="fa fa-reply" aria-hidden="true" style="float:right; margin-top:1px; margin-bottom: 2px; height:1px;"></i>
+           </br>
             </div>
         </div>
         <div class="m-messenger__message-pic">
@@ -76,67 +104,48 @@ function mapComment() {
 </div>`
         document.getElementById("mCSB_3_container").innerHTML = document.getElementById("mCSB_3_container").innerHTML + html
 
-        //         htmldata = `
-        //     <div class="m-messenger__wrapper commguy">
-        //     <div class="m-messenger__message m-messenger__message--in">
-        //         <div class="m-messenger__message-pic">
-        //             <img src="assets/app/media/img//users/user3.jpg" alt="" class="mCS_img_loaded">
-        //         </div>
-        //         <div class="m-messenger__message-body">
-        //             <div class="m-messenger__message-arrow"></div>
-        //             <div class="m-messenger__message-content">
-        //                 <div class="m-messenger__message-username">
-        //                     ${elem.name} wrote
-        //                     <span id="datee" style="float: right;"></span>
-        //                 </div>
-        //                 <div class="m-messenger__message-text">
-        //                     ${elem.comment}
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
-        //     `;
-        //         firstChild = getElementById("filler");
-
-        //         var e = document.createElement('div');
-        //         e.innerHTML = htmldata;
-
-        //         while (firstChild) {
-        //             firstChild.appendChild(htmldata);
-        //         }
-
-        //     document.getElementById("filler").innerHTML = document.getElementById("mCSB_3_container").innerHTML + `
-        //     <div class="m-messenger__wrapper commguy">
-        //     <div class="m-messenger__message m-messenger__message--in">
-        //         <div class="m-messenger__message-pic">
-        //             <img src="assets/app/media/img//users/user3.jpg" alt="" class="mCS_img_loaded">
-        //         </div>
-        //         <div class="m-messenger__message-body">
-        //             <div class="m-messenger__message-arrow"></div>
-        //             <div class="m-messenger__message-content">
-        //                 <div class="m-messenger__message-username">
-        //                     ${elem.name} wrote
-        //                     <span id="datee" style="float: right;"></span>
-        //                 </div>
-        //                 <div class="m-messenger__message-text">
-        //                     ${elem.comment}
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
-        //     `;
     })
 }
 
 function addComment() {
     var newObj = {
         name: "Chiamaka",
-        comment: document.getElementById("Textarea1").value,
-        id: 5
+        comment: document.getElementById("Textarea2").value,
+        id: 5,
+        date: "12/3/1990"
     }
-    console.log(newObj)
+    console.log(newObj);
     data.push(newObj);
-    mapComment()
+    mapComment();
+    document.getElementById("Textarea2").value = "";
+}
+
+function addReply() {
+    parentComment = document.getElementById("replydiv");
+    childComment = `<div class="m-messenger__wrapper" style=" margin-top:9px; padding-right: 10px; padding-left: 10px;">
+    <div class="m-messenger__message m-messenger__message--out">
+    
+        <div class="m-messenger__message-body">
+            <div class="m-messenger__message-arrow"></div>
+            <div class="m-messenger__message-content">
+            <div class="m-messenger__message-username">
+            <span class="secondary" style="float: left;"><strong>Dammy</strong></span>
+            <span id="datee" style="float: right;">12/2/2019</span>
+               
+                </div>
+
+                <div class="m-messenger__message-text" style="min-width: 250px; max-width: 300px; max-height: 4000px;">  <p> </br> 
+              <span style="width: 250px"> ${document.getElementById("replyTextId").value} </span>
+    </p>  
+                </div>
+                </br>
+            </div>
+        </div>
+        <div class="m-messenger__message-pic">
+        <img src="assets/app/media/img//users/user3.jpg" alt="" class="mCS_img_loaded">
+    </div>
+    </div>
+</div>`;
+    parentComment.innerHTML = parentComment.innerHTML + childComment;
+    document.getElementById("replyTextId").value = "";
 }
