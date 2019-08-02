985be1408a1658f3d8a5cfbe7603053fdd7bb632
tunde2010 <tundeawopegba@gmail.com>
2019-08-02 12:24:56 +0100

    Calendar

--- public/js/main.js
+++ public/js/main.js
@@ -49,4 +49,73 @@ $(document).ready(function () {
       $(this).addClass('active')
     }
   })
-})
+});
+$(function() {
+    let copyButtonTrans = 'copy';
+    let csvButtonTrans = 'csv';
+    let excelButtonTrans = 'excel';
+    let pdfButtonTrans = 'pdf';
+    let printButtonTrans = 'print';
+    let colvisButtonTrans = 'colvis';
+
+    let languages = {
+        'en': 'https://cdn.datatables.net/plug-ins/1.10.19/i18n/English.json'
+    };
+
+    $.extend(true, $.fn.dataTable.Buttons.defaults.dom.button, { className: 'btn' })
+    $.extend(true, $.fn.dataTable.defaults, {
+    columnDefs: [{
+        orderable: false,
+        className: 'select-checkbox',
+        targets: 0
+    }, {
+        orderable: false,
+        searchable: false,
+        targets: -1
+    }],
+        select: {
+        style:    'multi+shift',
+            selector: 'td:first-child'
+    },
+    order: [],
+        scrollX: true,
+        pageLength: 100,
+        dom: 'lBfrtip<"actions">',
+        buttons: [
+        {
+            extend: 'csv',
+            className: 'btn-default',
+            text: csvButtonTrans,
+            exportOptions: {
+                columns: ':visible'
+            }
+        },
+        {
+            extend: 'excel',
+            className: 'btn-default',
+            text: excelButtonTrans,
+            exportOptions: {
+                columns: ':visible'
+            }
+        },
+        {
+            extend: 'pdf',
+            className: 'btn-default',
+            text: pdfButtonTrans,
+            exportOptions: {
+                columns: ':visible'
+            }
+        },
+        {
+            extend: 'print',
+            className: 'btn-default',
+            text: printButtonTrans,
+            exportOptions: {
+                columns: ':visible'
+            }
+        }
+    ]
+});
+
+    $.fn.dataTable.ext.classes.sPageButton = '';
+});
--- resources/views/theme/laravel/dashboard.blade.php
+++ resources/views/theme/laravel/dashboard.blade.php
@@ -107,7 +107,7 @@
                             <div class="m--space-10"></div>
                             <div class="progress m-progress--sm">
                                 <div class="progress-bar m--bg-danger" role="progressbar"
-                                     style="width: {{ ($clients->whereYear('date_of_engagement', date('Y'))->count() * 100)/ count($clients) }}%;" aria-valuenow="50" aria-valuemin="0"
+                                     style="width: {{ ($clients->where('date_of_engagement', date('Y'))->count() * 100)/ count($clients) }}%;" aria-valuenow="50" aria-valuemin="0"
                                      aria-valuemax="100"></div>
                             </div>
                             <span class="m-widget24__change">New Addition</span>
@@ -182,9 +182,301 @@
         </div>
     </div>
     <!--End::Section Calendar-->
+
+    <!-- Begin: Client Datatable --->
+    <div class="row">
+        <div class="col-xl-12">
+            <div class="m-portlet m-portlet--mobile ">
+                <div class="m-portlet__head">
+                    <div class="m-portlet__head-caption">
+                        <div class="m-portlet__head-title">
+                            <h3 class="m-portlet__head-text">
+                                Client List
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="m-portlet__head-tools">
+                        <ul class="m-portlet__nav">
+                            <li class="m-portlet__nav-item">
+                                <div class="m-dropdown m-dropdown--inline m-dropdown--arrow m-dropdown--align-right m-dropdown--align-push"
+                                     data-dropdown-toggle="hover" aria-expanded="true">
+                                    <a href="#"
+                                       class="m-portlet__nav-link btn btn-lg btn-secondary  m-btn m-btn--icon m-btn--icon-only m-btn--pill  m-dropdown__toggle">
+                                        <i class="la la-ellipsis-h m--font-brand"></i>
+                                    </a>
+                                    <div class="m-dropdown__wrapper">
+                                        <span class="m-dropdown__arrow m-dropdown__arrow--right m-dropdown__arrow--adjust"></span>
+                                        <div class="m-dropdown__inner">
+                                            <div class="m-dropdown__body">
+                                                <div class="m-dropdown__content">
+                                                    <ul class="m-nav">
+                                                        <li class="m-nav__section m-nav__section--first">
+																				<span class="m-nav__section-text">
+																					Quick Actions
+																				</span>
+                                                        </li>
+                                                        <li class="m-nav__item">
+                                                            <a href="" class="m-nav__link">
+                                                                <i class="m-nav__link-icon flaticon-share"></i>
+                                                                <span class="m-nav__link-text">
+																						Create Post
+																					</span>
+                                                            </a>
+                                                        </li>
+                                                        <li class="m-nav__item">
+                                                            <a href="" class="m-nav__link">
+                                                                <i class="m-nav__link-icon flaticon-chat-1"></i>
+                                                                <span class="m-nav__link-text">
+																						Send Messages
+																					</span>
+                                                            </a>
+                                                        </li>
+                                                        <li class="m-nav__item">
+                                                            <a href="" class="m-nav__link">
+                                                                <i class="m-nav__link-icon flaticon-multimedia-2"></i>
+                                                                <span class="m-nav__link-text">
+																						Upload File
+																					</span>
+                                                            </a>
+                                                        </li>
+                                                        <li class="m-nav__section">
+																				<span class="m-nav__section-text">
+																					Useful Links
+																				</span>
+                                                        </li>
+                                                        <li class="m-nav__item">
+                                                            <a href="" class="m-nav__link">
+                                                                <i class="m-nav__link-icon flaticon-info"></i>
+                                                                <span class="m-nav__link-text">
+																						FAQ
+																					</span>
+                                                            </a>
+                                                        </li>
+                                                        <li class="m-nav__item">
+                                                            <a href="" class="m-nav__link">
+                                                                <i class="m-nav__link-icon flaticon-lifebuoy"></i>
+                                                                <span class="m-nav__link-text">
+																						Support
+																					</span>
+                                                            </a>
+                                                        </li>
+                                                        <li class="m-nav__separator m-nav__separator--fit m--hide"></li>
+                                                        <li class="m-nav__item m--hide">
+                                                            <a href="#"
+                                                               class="btn btn-outline-danger m-btn m-btn--pill m-btn--wide btn-sm">
+                                                                Submit
+                                                            </a>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="m-portlet__body">
+                    <!--begin: Datatable -->
+                    <div class="m_datatable" id="m_datatable_clients_list"></div>
+                    <!--end: Datatable -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End: Client Datatable -->
 @endsection
 @section('javascript')
 {{--    <script src="metro/assets/app/js/dashboard.js" type="text/javascript"></script>--}}
     @parent
+<script>
+    $(document).ready(function () {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        $.ajax({
+            type: "GET",
+            url: 'http://localhost/task-management/public/api/v1/tasks',
+            success: function (data) {
 
+            },
+            error: function (data) {
+                console.log('Error:', data);
+            }
+        });
+
+        //delete user login
+        $('body').on('click', '.delete-user', function () {
+            var user_id = $(this).data("id");
+            confirm("Are You sure want to delete !");
+
+            $.ajax({
+                type: "DELETE",
+                url: "{{ url('ajax-crud')}}"+'/'+user_id,
+                success: function (data) {
+                    $("#user_id_" + user_id).remove();
+                },
+                error: function (data) {
+                    console.log('Error:', data);
+                }
+            });
+        });
+    });
+
+    var datatableClient = function() {
+        if ($('#m_datatable_clients_list').length === 0) {
+            return;
+        }
+
+        var datatable = $('.m_datatable').mDatatable({
+            data: {
+                type: 'remote',
+                source: {
+                    read: {
+                        url: 'http://localhost/task-management/public/api/v1/clients/',
+                        method: 'GET',
+                        headers: {
+                            'X-CSRF-TOKEN': '{{ csrf_token() }}',
+                        },
+                        contentType: 'application/json',
+                    }
+                },
+                pageSize: 10,
+                saveState: {
+                    cookie: false,
+                    webstorage: true
+                },
+                serverPaging: true,
+                serverFiltering: true,
+                serverSorting: true
+            },
+
+            layout: {
+                theme: 'default',
+                class: '',
+                scroll: true,
+                height: 380,
+                footer: false
+            },
+
+            sortable: true,
+
+            filterable: false,
+
+            pagination: true,
+            search: {
+                input: $('#generalSearch'),
+                onEnter: true,
+            },
+            toolbar: {
+                layout: ['pagination', 'info']
+            },buttons: [
+                'copy', 'excel', 'pdf'
+            ],
+            columns: [
+                {
+                field: "id",
+                title: "#",
+                sortable: false,
+                width: 40,
+                selector: {
+                    class: 'm-checkbox--solid m-checkbox--brand'
+                },
+                textAlign: 'center'
+            }, {
+                field: "name",
+                title: "Client",
+                sortable: 'asc',
+                filterable: true,
+                width: 150,
+            }, {
+                field: "phone",
+                title: "Phone",
+                width: 150,
+            }, {
+                field: "email",
+                title: "Email",
+                width: 150,
+                responsive: {
+                    visible: 'lg'
+                }
+            },{
+                field: "address",
+                title: "Address",
+                width: 150,
+                responsive: {
+                    visible: 'lg'
+                }
+            },{
+                field: "date_of_engagement",
+                title: "Engaged",
+                width: 100,
+            }, {
+                field: "expiry_date",
+                title: "Expire",
+                width: 100,
+            },  {
+                field: "Actions",
+                width: 110,
+                title: "Actions",
+                sortable: false,
+                overflow: 'visible',
+                template: function(row) {
+                    var dropup = (row.getDatatable().getPageSize() - row.getIndex()) <= 4 ? 'dropup' : '';
+                    return '\<a href="#" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="Edit details">\
+                            <i class="la la-edit"></i>\
+                        </a>\
+                        <a href="" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill" title="Delete">\
+                            <i class="la la-trash"></i>\
+                        </a>\
+                    ';
+                }
+            }],
+        });
+    };
+    // datatables for Clients
+    datatableClient();
+
+    var calendarInit = function() {
+        if ($('#m_calendar').length === 0) {
+            return;
+        }
+
+        var todayDate = moment().startOf('day');
+        var YM = todayDate.format('YYYY-MM');
+        var YESTERDAY = todayDate.clone().subtract(1, 'day').format('YYYY-MM-DD');
+        var TODAY = todayDate.format('YYYY-MM-DD');
+        var TOMORROW = todayDate.clone().add(1, 'day').format('YYYY-MM-DD');
+        let events = {!! json_encode($events) !!};
+        $('#m_calendar').fullCalendar({
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay,listWeek'
+            },
+            editable: true,
+            eventLimit: true, // allow "more" link when too many events
+            navLinks: true,
+            defaultDate: moment(),
+            events : events,
+
+            eventRender: function(event, element) {
+                if (element.hasClass('fc-day-grid-event')) {
+                    element.data('content', event.description);
+                    element.data('placement', 'top');
+                    mApp.initPopover(element);
+                } else if (element.hasClass('fc-time-grid-event')) {
+                    element.find('.fc-title').append('<div class="fc-description">' + event.description + '</div>');
+                } else if (element.find('.fc-list-item-title').lenght !== 0) {
+                    element.find('.fc-list-item-title').append('<div class="fc-description">' + event.description + '</div>');
+                }
+            }
+        });
+    }
+
+    calendarInit();
+</script>
 @endsection
