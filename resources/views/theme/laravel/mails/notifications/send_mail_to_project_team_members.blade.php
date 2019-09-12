1cf652d02423913f2dc0b5617d59bf95d4cf4f58
tunde2010 <tundeawopegba@gmail.com>
2019-09-12 19:48:15 +0100

    Create a mail notification for project

--- resources/views/theme/laravel/mails/notifications/send_mail_to_project_team_members.blade.php
+++ resources/views/theme/laravel/mails/notifications/send_mail_to_project_team_members.blade.php
@@ -5,8 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
-
+    <title>Task Management</title>
+    <link href="{{ url('metro/assets/vendors/base/vendors.bundle.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ url('metro/assets/demo/demo2/base/style.bundle.css') }}" rel="stylesheet" type="text/css"/>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
@@ -46,10 +47,10 @@
         }
 
         .title {
-            font-size: 84px;
+            font-size: 24px;
         }
 
-        .links > a {
+        .links > a, p {
             color: #636b6f;
             padding: 0 25px;
             font-size: 13px;
@@ -67,21 +68,23 @@
 <body>
 <div class="flex-center position-ref full-height">
     <div class="content">
-        Hello!
+        <div class="title">Hello!</div>
 
         <p>You have been added to work on a project.   </p>
         <p>See below for the project details:</p>
         <br>
-        <table>
-            <tr>
-                <td>Project Name: </td>
-                <td>{{ $project->name }}</td>
-            </tr>
-            <tr>
-                <td>Project Manager: </td>
-                <td>{{ $project->manager->name. ' ('.$project->manager->email . ')'  }}</td>
-            </tr>
-        </table>
+        <div class="row title">
+            <table>
+                <tr>
+                    <td>Project Name: </td>
+                    <td>{{ $project->name }}</td>
+                </tr>
+                <tr>
+                    <td>Project Manager: </td>
+                    <td>{{ $project->manager->name. ' ('.$project->manager->email . ')'  }}</td>
+                </tr>
+            </table>
+        </div>
 
         <div class="row">
             <table class="table table-bordered" border="1">
@@ -102,6 +105,7 @@
                 </tbody>
             </table>
         </div>
+        <br>
         <div class="links">
             <a href="https://portal.ipaysuite.com">iPaySuite</a>
             <a href="https://stransact.com">Stransact</a>
