7c9720d5834ef8eadf0c81474ef5c118c179253b
tunde2010 <tundeawopegba@gmail.com>
2019-10-16 12:38:01 +0100

    Fix Document API Error

--- app/MyMedia.php
+++ app/MyMedia.php
@@ -4,7 +4,6 @@ namespace App;
 
 use App\Traits\Auditable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia\HasMedia;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
