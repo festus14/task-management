3ad39650e387e23052bbd0a0ce60dfe26fd301f0
festus14 <festusomole14@gmail.com>
2019-10-24 12:57:34 +0100

    Fixing edit team_members on project page

--- resources/views/pages/js/project_scripts/projecttype_subtype_js.blade.php
+++ resources/views/pages/js/project_scripts/projecttype_subtype_js.blade.php
@@ -1,88 +1,4 @@
 <script>
-    function displayAddPsubtype() {
-    $("#subtypemainModal").modal('show');
-    $.ajax({
-        type: "GET",
-        url: '{{ url("/api/v1/project-types") }}',
-        success: function (data) {
-            let subtypemainModalBody = document.getElementById('subtypemainModalBody');
-            subtypemainModalBody.innerHTML = `
-        <form id="addprojsubtypeform2" enctype="multipart/form-data">
-            @csrf
-                <div  class="modal-body">
-
-                    <div class="form-group">
-                        <label for="project-type">Select Project Type</label>
-                        <select id="project-type" name="project_type_id" class="selectDesign form-control">
-                            <option value="" selected></option>
-                                ${data.data.map(elem => `<option value="${elem.id}">${elem.name}</option>`)}
-                    </select>
-                    <div class="error" id="projectTTTypeErr"></div>
-                </div>
-
-                <div class="form-group">
-                    <label for="create-task">Subtype Name</label>
-                    <input type="text" class="form-control" name="name" id="sub-type" placeholder="">
-                    <div class="error" id="projectSubTypeErr"></div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" onclick="$('#subtypemainModal').modal('hide');" data-target="#subtypemainModal">Close</button>
-                <input type="button" class="btn btn-danger" style="background-color:#8a2a2b; color:white;" onclick="validateProjectSubType();" value="{{ trans('global.create') }}">
-            </div>
-        </form>
-
-                        `
-        },
-
-        error: function (data) {
-            console.log('Error:', data);
-        }
-    });
-    }
-
-function displayAddPsubtypeOut() {
-    $("#subtypemainModal").modal('show');
-    $.ajax({
-        type: "GET",
-        url: '{{ url("/api/v1/project-types") }}',
-        success: function (data) {
-            let subtypemainModalBody = document.getElementById('subtypemainModalBody');
-            subtypemainModalBody.innerHTML = `
-        <form id="addprojsubtypeform2" enctype="multipart/form-data">
-            @csrf
-                <div  class="modal-body">
-
-                    <div class="form-group">
-                        <label for="project-type">Select Project Type</label>
-                        <select id="project-type" name="project_type_id" class="selectDesign form-control">
-                            <option value="" selected></option>
-                                ${data.data.map(elem => `<option value="${elem.id}">${elem.name}</option>`)}
-                    </select>
-                    <div class="error" id="projectTTTypeErr"></div>
-                </div>
-
-                <div class="form-group">
-                    <label for="create-task">Subtype Name</label>
-                    <input type="text" class="form-control" name="name" id="sub-type" placeholder="">
-                    <div class="error" id="projectSubTypeErr"></div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" onclick="$('#subtypemainModal').modal('hide');" data-target="#subtypemainModal">Close</button>
-                <input type="button" class="btn btn-danger" style="background-color:#8a2a2b; color:white;" onclick="validateProjectSubTypeOut();" value="{{ trans('global.create') }}">
-            </div>
-        </form>
-
-                        `
-        },
-
-        error: function (data) {
-            console.log('Error:', data);
-        }
-    });
-}
-
     function ProjectTypeSubmit() {
         $.ajax({
             type: "POST",
@@ -146,9 +62,6 @@ function displayAddPsubtypeOut() {
                     icon: "success",
                     confirmButtonColor: "#DD6B55",
                 });
-                window.setTimeout(function() {
-                    $("#kt_table_project_type").DataTable().ajax.reload();
-                }, 2300)
             },
             error: function(error) {
                 swal({
@@ -165,9 +78,8 @@ function displayAddPsubtypeOut() {
 
     function submitEditProjectForm(proID) {
         let formData = $('#editProjectform').serialize();
-        console.log(formData)
         $.ajax({
-            type: "PATCH",
+            type: "PUT",
             url: "{{ url('/api/v1/projects') }}" + "/" + proID,
             data: formData,
             success: function(data) {
@@ -262,9 +174,6 @@ function displayAddPsubtypeOut() {
                     icon: "success",
                     confirmButtonColor: "#DD6B55",
                 });
-                window.setTimeout(function() {
-                    $("#kt_table_project_subtype").DataTable().ajax.reload();
-                }, 2300)
 
             },
             error: function(error) {
@@ -382,135 +291,6 @@ function displayAddPsubtypeOut() {
             }
         });
     }
-
-    //  Edit Project Sub form
-    var editSubData
-
-function editProjectSubtype(sub_id) {
-
-    $.ajax({
-                type: "GET",
-                url: "/api/v1/project-sub-types",
-                success: function(data) {
-                        let editProjectSubTypeModalBody = document.getElementById('editProjectSubTypeModalBody');
-                        editProjectSubTypeModalBody.innerHTML = `
-<form id="editProjectSubtypeForm" enctype="multipart/form-data">
-    @csrf
-                <div  class="modal-body">
-
-                        <div class="form-group">
-                            <label for="project-type">Select Project Type</label>
-                            <select id="projecTttype" name="project_type_id" class="form-control" required>
-                            ${data.data.map(elem => `<option value="${elem.project_type.id}">${elem.project_type.name}</option>`)}
-                            </select>
-                            <div class="error" id="editProjectTTTypeErr"></div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="create-task">Subtype Name</label>
-                            <input type="text" class="form-control" name="name" id="subTtype">
-                            <div class="error" id="editProjectSubTypeErr"></div>
-                        </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" onclick="$('#editProjectSubTypeModal').modal('hide');" data-target="#subtypemainModal">Close</button>
-                    <input type="button" class="btn btn-danger" style="background-color:#8a2a2b; color:white;" onclick="ValidateEditProjectSubType(${sub_id});" value="Update">
-                </div>
-                </form>
-        `
-        },
-
-        error: function (data) {
-            console.log('Error:', data);
-        }
-
-    })
-
-    $.ajax({
-        type: "GET",
-        url: "/api/v1/project-sub-types/" + sub_id,
-        success: function (data) {
-            editSubData = data.data;
-            $('#projecTttype').val(editSubData[0].project_type_id + "");
-            $('#subTtype').val(editSubData[0].name);
-        },
-
-        error: function (data) {
-            console.log('Error:', data);
-        }
-
-    })
-
-}
-
-//Edit Project Type
-var editProjectTypeData;
-
-function editProjectType(type_id) {
-    $.ajax({
-        type: "GET",
-        url: "/api/v1/project-types" + "/" + type_id,
-        success: function (data) {
-            editProjectTypeData = data.data;
-            $('#editprojTypeInput').val(editProjectTypeData.name);
-        },
-
-        error: function (data) {
-            console.log('Error:', data);
-        }
-
-
-    })
-
-
-    let editProjectTypeBody = document.getElementById('editProjectTypeBody');
-    editProjectTypeBody.innerHTML = `
-<form id="editProtypeform" enctype="multipart/form-data">
-    @csrf
-        <div class="modal-body">
-            <div class="form-group">
-                <label for="create-task">Project type name</label>
-                <input type="text" class="form-control" id="editprojTypeInput" name="name" placeholder="" value="" required>
-                <div class="error" id="editProjectTypeErr"></div>
-            </div>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" onclick="$('#EditProjectTypeModal').modal('hide');">Close</button>
-            <input class="btn btn-danger" type="button" style="background-color:#8a2a2b; color:white;" onclick="validateEditProjectType(${type_id})" value="Update">
-    </div>
-</form>
-`
-}
-
-function submitEditProjectType(typeId) {
-    $.ajax({
-        type: "PUT",
-        data: $('#editProtypeform').serialize(),
-        url: '/api/v1/project-types' + '/' + typeId,
-        success: function (data) {
-            swal({
-                title: "Success!",
-                text: "Project type edited!",
-                icon: "success",
-                confirmButtonColor: "#DD6B55",
-            });
-            $('#EditProjectTypeModal').modal('hide');
-            window.setTimeout(function () {
-                $("#kt_table_project_type").DataTable().ajax.reload();
-            }, 2300)
-
-        },
-        error: function (error) {
-            swal({
-                title: "Project type editing failed!",
-                icon: "error",
-                confirmButtonColor: "#fc3",
-                confirmButtonText: "OK",
-            });
-        }
-    });
-}
-
 
     //  Function for deleting single project
     function deleteProjectType(proID) {
