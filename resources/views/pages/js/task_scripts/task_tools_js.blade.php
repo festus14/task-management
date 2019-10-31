b3019c8f2c63f1c5be469185881afeaea2f58d01
tunde2010 <tundeawopegba@gmail.com>
2019-10-31 10:18:34 +0100

    re-organize the code(task_tools_js)

--- resources/views/pages/js/task_scripts/task_tools_js.blade.php
+++ resources/views/pages/js/task_scripts/task_tools_js.blade.php
@@ -356,52 +356,52 @@
         }
 
         Dropzone.options.documentDropzone = {
-        url: '{{ route('admin.task-documents.storeMedia') }}',
-        maxFilesize: 20, // MB
-        maxFiles: 1,
-        addRemoveLinks: true,
-        headers: {
-        'X-CSRF-TOKEN': "{{ csrf_token() }}"
-        },
-        params: {
-        size: 20
-        },
-        success: function (file, response) {
-        $('form').find('input[name="document"]').remove()
-        $('form').append('<input type="hidden" name="document" value="' + response.name + '">')
-        },
-        removedfile: function (file) {
-        file.previewElement.remove()
-        $('form').find('input[name="document"]').remove()
-        this.options.maxFiles = this.options.maxFiles + 1
-        },
-        init: function () {
-            @if(isset($taskDocument) && $taskDocument->document)
-            var file = {!! json_encode($taskDocument->document) !!}
-                this.options.addedfile.call(this, file)
-            file.previewElement.classList.add('dz-complete')
-            $('form').append('<input type="hidden" name="document" value="' + file.file_name + '">')
-            this.options.maxFiles = this.options.maxFiles - 1
+            url: '{{ route('admin.task-documents.storeMedia') }}',
+            maxFilesize: 20, // MB
+            maxFiles: 1,
+            addRemoveLinks: true,
+            headers: {
+                'X-CSRF-TOKEN': "{{ csrf_token() }}"
+            },
+            params: {
+                size: 20
+            },
+            success: function (file, response) {
+                $('form').find('input[name="document"]').remove()
+                $('form').append('<input type="hidden" name="document" value="' + response.name + '">')
+            },
+            removedfile: function (file) {
+                file.previewElement.remove()
+                $('form').find('input[name="document"]').remove()
+                this.options.maxFiles = this.options.maxFiles + 1
+            },
+            init: function () {
+                @if(isset($taskDocument) && $taskDocument->document)
+                var file = {!! json_encode($taskDocument->document) !!}
+                    this.options.addedfile.call(this, file)
+                file.previewElement.classList.add('dz-complete')
+                $('form').append('<input type="hidden" name="document" value="' + file.file_name + '">')
+                this.options.maxFiles = this.options.maxFiles - 1
 
-                @endif
+                    @endif
             },
             error: function (file, response) {
-            if ($.type(response) === 'string') {
-                var message = response //dropzone sends it's own error messages in string
-            } else {
-                var message = response.errors.file
-            }
-            file.previewElement.classList.add('dz-error')
-            _ref = file.previewElement.querySelectorAll('[data-dz-errormessage]')
-            _results = []
-            for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-                node = _ref[_i]
-                _results.push(node.textContent = message)
-            }
+                if ($.type(response) === 'string') {
+                    var message = response //dropzone sends it's own error messages in string
+                } else {
+                    var message = response.errors.file
+                }
+                file.previewElement.classList.add('dz-error')
+                _ref = file.previewElement.querySelectorAll('[data-dz-errormessage]')
+                _results = []
+                for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                    node = _ref[_i]
+                    _results.push(node.textContent = message)
+                }
 
-            return _results
-        }
-        }
+                return _results
+            }
+        };
         function switch_doc_type(type) {
             switch (type) {
                 case '1':
